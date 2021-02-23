436e9f7da6495be97a9fe0e4fe475030453ac12b
Abbazov Valerian <asr77734@gmail.com>
2021-02-23 18:11:31 +0500

    1_2 update

--- Task1_2/main.c
+++ Task1_2/main.c
@@ -1,6 +1,7 @@
 #include "main.h"
 int main(void){
 	RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
+	GPIOB->OSPEEDR &=~ 0x00013333;
 	GPIOB->MODER |= GPIO_MODER_MODER0_0 | GPIO_MODER_MODER1_0 
 								| GPIO_MODER_MODER2_0 | GPIO_MODER_MODER7_0 
 								| GPIO_MODER_MODER6_0 | GPIO_MODER_MODER5_0 
@@ -8,15 +9,17 @@ int main(void){
 								| GPIO_MODER_MODER8_0;
 	GPIOB->MODER&=~ GPIO_MODER_MODER15;
 	
-	unsigned  regOn[8] = {0x101, 0x002, 0x004, 0x080, 0x040, 0x020, 0x010, 0x008};  
-	unsigned  regOff[8] = {0x1fe, 0x1fd, 0x1fb, 0x17f, 0x1bf, 0x1df, 0x1ef, 0x0f7};
+	unsigned  reg[8] = {0x101, 0x002, 0x004, 0x080, 0x040, 0x020, 0x010, 0x008};  
+	//unsigned  regOff[8] = {0x1fe, 0x1fd, 0x1fb, 0x17f, 0x1bf, 0x1df, 0x1ef, 0x0f7};
 	
 	if(1){
 		for (uint32_t i = 0; i<9; i++){
-			alllamp(regOn[i]);
+			GPIOB->ODR |= reg[i];
+			delay(100000);
 		}
 		for (uint32_t j = 0; j<9; j++){
-			alllamp(regOff[j]);
+			GPIOB->ODR |=~ reg[j];
+			delay(100000);
 		}
 	}
 }
@@ -25,9 +28,4 @@ void delay(uint32_t count)
 {
 	volatile uint32_t i;
 	for (i =0;i<count;i++);
-}
-
-void alllamp(unsigned reg){
-		GPIOB->ODR |= reg;
-		delay(100000);
 }
--- Task1_2/main.h
+++ Task1_2/main.h
@@ -1,3 +1,3 @@
 #include "stm32f0xx.h"                  // Device header
 void delay(uint32_t);
-void alllamp(unsigned);
+
