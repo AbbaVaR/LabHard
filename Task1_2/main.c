78ee2cf47a0e568c881da02b88fa7061e6fb4d9c
Abbazov Valerian <asr77734@gmail.com>
2021-02-23 13:27:00 +0500

    lab 1_2 working

--- Task1_2/main.c
+++ Task1_2/main.c
@@ -1,42 +1,24 @@
 #include "main.h"
 int main(void){
 	RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
-	GPIOB->MODER |= GPIO_MODER_MODER0_0 | GPIO_MODER_MODER1_0 | GPIO_MODER_MODER2_0 | GPIO_MODER_MODER7_0 | GPIO_MODER_MODER6_0  | GPIO_MODER_MODER5_0 | GPIO_MODER_MODER4_0|  GPIO_MODER_MODER3_0 | GPIO_MODER_MODER8_0;
+	GPIOB->MODER |= GPIO_MODER_MODER0_0 | GPIO_MODER_MODER1_0 
+								| GPIO_MODER_MODER2_0 | GPIO_MODER_MODER7_0 
+								| GPIO_MODER_MODER6_0 | GPIO_MODER_MODER5_0 
+								| GPIO_MODER_MODER4_0	|  GPIO_MODER_MODER3_0 
+								| GPIO_MODER_MODER8_0;
 	GPIOB->MODER&=~ GPIO_MODER_MODER15;
 	
-	GPIOB->ODR |=0x101; //0
-	delay(500000);
-	GPIOB->ODR |=0x102; //1
-	delay(500000);
-	GPIOB->ODR |=0x104; //2
-		delay(500000);
-	GPIOB->ODR |=0x180; //7
-		delay(500000);
-	GPIOB->ODR |=0x140; //6
-		delay(500000);
-	GPIOB->ODR |=0x120; //5
-		delay(500000);
-	GPIOB->ODR |=0x110; //4
-		delay(500000);
-	GPIOB->ODR |=0x108; //3
-		delay(500000);
-	GPIOB->ODR &=0x1fe; //0
-		delay(500000);
-	GPIOB->ODR &=0x1fd; //1
-		delay(500000);
-	GPIOB->ODR &=0x1fb; //2
-		delay(500000);
-	GPIOB->ODR &=0x17f; //7
-		delay(500000);
-	GPIOB->ODR &=0x1bf; //6
-		delay(500000);
-	GPIOB->ODR &=0x1df; //5
-		delay(500000);
-	GPIOB->ODR &=0x1ef; //4
-		delay(500000);
-	GPIOB->ODR &=0x0f7; //3
-
-
+	unsigned  regOn[8] = {0x101, 0x002, 0x004, 0x080, 0x040, 0x020, 0x010, 0x008};  
+	unsigned  regOff[8] = {0x1fe, 0x1fd, 0x1fb, 0x17f, 0x1bf, 0x1df, 0x1ef, 0x0f7};
+	
+	if(1){
+		for (uint32_t i = 0; i<9; i++){
+			alllamp(regOn[i]);
+		}
+		for (uint32_t j = 0; j<9; j++){
+			alllamp(regOff[j]);
+		}
+	}
 }
 
 void delay(uint32_t count)
@@ -45,4 +27,7 @@ void delay(uint32_t count)
 	for (i =0;i<count;i++);
 }
 
-
+void alllamp(unsigned reg){
+		GPIOB->ODR |= reg;
+		delay(100000);
+}
