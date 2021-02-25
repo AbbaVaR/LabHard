8a9ae5ddcc9ecd1ebe97636826b5d11fcfdb62e8
Abbazov Valerian <asr77734@gmail.com>
2021-02-25 10:22:34 +0500

    1_2 1

--- Task1_2/main.c
+++ Task1_2/main.c
@@ -10,18 +10,19 @@ int main(void){
 	GPIOB->MODER&=~( GPIO_MODER_MODER12 | GPIO_MODER_MODER13 |  GPIO_MODER_MODER14 |  GPIO_MODER_MODER15);
 	
 	unsigned  reg[8] = {0x101, 0x002, 0x004, 0x080, 0x040, 0x020, 0x010, 0x008};  
-	//unsigned  regOff[8] = {0x1fe, 0x1fd, 0x1fb, 0x17f, 0x1bf, 0x1df, 0x1ef, 0x0f7};
+		if( ((GPIOB->IDR)&0x8000>>15) == 1){
+			for (uint32_t i = 0; i<9; i++){
+				GPIOB->ODR |= reg[i];
+				
+			}
+			/*for (uint32_t j = 0; j<9; j++){
+				GPIOB->ODR &=~ reg[j];
+				delay(10000);
+			}*/
+		}
 	
-	if(((GPIOB->IDR)&0x8000) == 1){
-		for (uint32_t i = 0; i<9; i++){
-			GPIOB->ODR |= reg[i];
-			delay(100000);
-		}
-		for (uint32_t j = 0; j<9; j++){
-			GPIOB->ODR &=~ reg[j];
-			delay(100000);
-		}
-	}
+
+	
 }
 
 void delay(uint32_t count)
@@ -29,3 +30,4 @@ void delay(uint32_t count)
 	volatile uint32_t i;
 	for (i =0;i<count;i++);
 }
+/*(GPIOB->IDR)&0x8000) == 1*/ 
