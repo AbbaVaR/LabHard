33a943267f29728e095ad0bd7b39ef577fb6c0e9
Abbazov Valerian <asr77734@gmail.com>
2021-02-25 09:21:18 +0500

    1_2

--- Task1_2/main.c
+++ Task1_2/main.c
@@ -7,12 +7,12 @@ int main(void){
 								| GPIO_MODER_MODER6_0 | GPIO_MODER_MODER5_0 
 								| GPIO_MODER_MODER4_0	|  GPIO_MODER_MODER3_0 
 								| GPIO_MODER_MODER8_0;
-	GPIOB->MODER&=~ GPIO_MODER_MODER15;
+	GPIOB->MODER&=~( GPIO_MODER_MODER12 | GPIO_MODER_MODER13 |  GPIO_MODER_MODER14 |  GPIO_MODER_MODER15);
 	
 	unsigned  reg[8] = {0x101, 0x002, 0x004, 0x080, 0x040, 0x020, 0x010, 0x008};  
 	//unsigned  regOff[8] = {0x1fe, 0x1fd, 0x1fb, 0x17f, 0x1bf, 0x1df, 0x1ef, 0x0f7};
 	
-	if(1){
+	if(((GPIOB->IDR)&0x8000) == 1){
 		for (uint32_t i = 0; i<9; i++){
 			GPIOB->ODR |= reg[i];
 			delay(100000);
