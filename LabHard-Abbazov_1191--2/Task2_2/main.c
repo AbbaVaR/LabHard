4107fa2678190fd5f71f4e8eae84d6a2dbf33ca1
Валерьян Аббазов <asr77734@gmail.com>
2021-05-07 15:00:03 +0500

    lab 2.2 in progress

--- LabHard-Abbazov_1191--2/Task2_2/main.c
+++ LabHard-Abbazov_1191--2/Task2_2/main.c
@@ -9,34 +9,68 @@ int main(void)
 								| GPIO_MODER_MODER6_0 | GPIO_MODER_MODER5_0 
 								| GPIO_MODER_MODER4_0	|  GPIO_MODER_MODER3_0 
 								| GPIO_MODER_MODER8_0;
-	unsigned reg[8] = {0x101, 0x102, 0x104, 0x180, 0x140, 0x120, 0x110, 0x108};  //Регистры для запуска светодиодов
-	unsigned input [8][8] = {{0x31,0}, {0x32,1}, {0x33,2}, {0x34,3}, {0x35,4}, {0x36,5}, {0x37,6}, {0x38,7}};
+	unsigned reg[8] = {0x1, 0x2, 0x4, 0x80, 0x40, 0x20, 0x10, 0x8};  //Регистры для запуска светодиодов
+	unsigned input [9][9] = {{0x31,0}, {0x32,1}, {0x33,2}, {0x34,3}, {0x35,4}, {0x36,5}, {0x37,6}, {0x38,7}, {0x30,8}};
 	uint16_t check[8] = {0,0,0,0,0,0,0,0};
 	unsigned elem =0;
+	GPIOB->BSRR |= 0x100;
 	while(1){
 		while ((USART1->ISR & USART_ISR_RXNE) == 0) { }
 		uint8_t d = (uint8_t)USART1->RDR;
-
-		while ((USART1->ISR & USART_ISR_TXE) == 0) { }
-		USART1->TDR = d;
 		
 		for (uint16_t i = 0; i < 9; i++){
 			if (d == input[i][0]){
 				elem = input[i][1];
 			}
 		}
-	
-			if (check[elem] == 0){
-				GPIOB->BSRR |= reg[elem];
-				check[elem] = 1;
-				d = 0x0;
+		
+		if (elem == 8){
+			for (int16_t i = 0; i<8; i++){
+				GPIOB->BSRR = reg[i] << 16;
+				check[i] = 0;
 			}
-			else{
-				GPIOB->BSRR |= reg[elem]<<16;
-				check[elem] = 0;
-				d = 0x0;
-			}			
+			continue;
+		}
+		
+		if (check[elem] == 0){
+			GPIOB->BSRR |= reg[elem];
+			check[elem] = 1;
+			while ((USART1->ISR & USART_ISR_TXE) == 0) { }
+			msg(elem, check[elem]);
+			continue;
+		}
+		else{
+			GPIOB->BSRR |= reg[elem]<<16;
+			check[elem] = 0;
+			d = 0x0;
+			while ((USART1->ISR & USART_ISR_TXE) == 0) { }
+			msg(elem, check[elem]);
+		  continue;
+		}
+		
 	}
+}
+
+void msg (unsigned number, unsigned flag){
+	uint8_t text1[6] = {0x44, 0x69, 0x6f, 0x64, 0xe0,0x20};
+	uint8_t text2[4] = {0x6f, 0x6e, 0xD, 0xA};
+	uint8_t text3[5] = {0x6f, 0x66,0x66, 0xD, 0xA};
+
+	/*	for (uint16_t i=0; i<6; i++){
+			USART1->TDR = text1[i];
+		}
+		USART1->TDR = (uint8_t)number;*/
+		
+		if (flag == 1){
+			for (uint16_t i=0; i<4; i++){
+			USART1->TDR = text2[i];
+			}
+		}
+		else {
+			for (uint16_t i=0; i<5; i++){
+			USART1->TDR = text3[i];		
+			}
+		}
 }
 
 void InitUSART1(){
